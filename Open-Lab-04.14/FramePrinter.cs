aad734218411d872c8aaaff03e798c2020352ee3
Ozaniak <Ozaniak@PC>
2020-10-26 23:20:51 +0100

    hotovo

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -6,7 +6,45 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            var longest = GetLongest(strings);
+            for (int i = 0; i < longest + 4; i++)
+            {
+                Console.Write("*");
+
+            }
+            Console.WriteLine();
+
+            foreach (string str in strings)
+            {
+                string line = $"* {str} ";
+                for (int i = 0; i < longest - str.Length; i++)
+                {
+                    line += " ";
+                }
+                Console.WriteLine(line + "*");
+            }
+
+            for (int i = 0; i < longest + 4; i++)
+            {
+                Console.Write("*");
+
+            }
+            Console.WriteLine();
+        }
+
+        public int GetLongest(string[] strings)
+        {
+            string temp = "";
+            int num = 0;
+            foreach (string str in strings)
+            {
+                if (str.Length > num)
+                {
+                    temp = str;
+                    num = str.Length;
+                }
+            }
+            return temp.Length;
         }
     }
 }
